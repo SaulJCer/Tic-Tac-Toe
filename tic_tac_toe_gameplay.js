da1f059d562bf9feee3b9d62038fe98432bd3784
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2026-05-31 15:31:58 -0400

    Added event listeners for all spots on gameboard

--- tic_tac_toe_gameplay.js
+++ tic_tac_toe_gameplay.js
@@ -1,6 +1,9 @@
 
 function Gameboard(player1, player2) {
     
+
+    const gameBoardArray= new Array(9);
+
     currentPlayer = player1; 
     function nextPlayersTurn () {
         if (currentPlayer === player1) {
@@ -22,12 +25,84 @@ function Gameboard(player1, player2) {
     const spot8 = document.getElementById("index8");
 
     spot0.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
         this.textContent = currentPlayer.marker; 
-        console.log("SPOT 0 was pressed");
+        // console.log("SPOT 0 was pressed");
+        gameBoardArray[0] = currentPlayer.marker;
+        // console.log(`This is index 0 value ${gameBoardArray[0]}`);
         nextPlayersTurn();
     });
     
-    return new Array(9);
+    spot1.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[1] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot2.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[2] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot3.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[3] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot4.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[4] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot5.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[5] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot6.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[6] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot7.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[7] = currentPlayer.marker;
+        nextPlayersTurn();
+    });
+    spot8.addEventListener("click", function (e) {
+        if (this.textContent != "") {
+            return (alert("SPOT HAS BEEN SELECTED, please choose another"))
+        }
+        this.textContent = currentPlayer.marker; 
+        gameBoardArray[8] = currentPlayer.marker;
+        nextPlayersTurn();
+        console.log(gameBoardArray)
+    });
+
+    return gameBoardArray;
+    
 }
 
 function Player(name, marker) {
@@ -37,10 +112,14 @@ function Player(name, marker) {
 }
 
 function Game() {
+
     const player1 = new Player("Rober", "X");
     const player2 = new Player("chicken", "Y")
     let newGameBoard = Gameboard(player1, player2);
 
+    
+
+    console.log(newGameBoard);
 }
 
 
